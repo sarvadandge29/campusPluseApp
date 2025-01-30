2dc6405a69c35e9e743f3eb8b1d0494559aef64c
sarvadandge29 <sarvadandge@gmail.com>
2025-01-30 16:30:03 +0530

    Layout of find teammates

--- app/(root)/(findTeammates)/findFlatmates.jsx
+++ app/(root)/(findTeammates)/findFlatmates.jsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const FindFlatemets = () => {
+const FindFlatmates = () => {
   return (
     <View>
       <Text>FindFlatemets</Text>
@@ -9,4 +9,4 @@ const FindFlatemets = () => {
   )
 }
 
-export default FindFlatemets
+export default FindFlatmates
