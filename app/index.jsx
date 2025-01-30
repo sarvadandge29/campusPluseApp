52d0a57e7fb83a8dc93aba85a8c708faeef7953c
sarvadandge29 <sarvadandge@gmail.com>
2025-01-30 12:49:28 +0530

    index change

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,9 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text } from 'react-native'
 
 const Index = () => {
   return (
     <View className="justify-center items-center flex-1">
-      <Text>Index</Text>
     </View>
   )
 }
