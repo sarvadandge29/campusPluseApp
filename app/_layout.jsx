0502d4c5bdeb215ea2ce170099c876614a0df601
sarvadandge29 <sarvadandge@gmail.com>
2025-01-30 23:20:02 +0530

    Add context provider

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,46 +1,41 @@
-import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router'
-import React, { useEffect } from 'react'
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import React, { useEffect } from "react";
 import "../global.css";
-import { StatusBar } from 'expo-status-bar';
+import AuthProvider from "../context/AuthContext";
 
 const AppLayout = () => {
     const [fontsLoaded, error] = useFonts({
-        'IBMPlexSans-Regular': require('../assets/fonts/IBMPlexSans-Regular.ttf'),
-        'IBMPlexSans-Medium': require('../assets/fonts/IBMPlexSans-Medium.ttf'),
-        'IBMPlexSans-SemiBold': require('../assets/fonts/IBMPlexSans-SemiBold.ttf'),
-        'IBMPlexSans-Bold': require('../assets/fonts/IBMPlexSans-Bold.ttf'),
-        'bebasNeueRegular': require('../assets/fonts/BebasNeue-Regular.ttf'),
+        "IBMPlexSans-Regular": require("../assets/fonts/IBMPlexSans-Regular.ttf"),
+        "IBMPlexSans-Medium": require("../assets/fonts/IBMPlexSans-Medium.ttf"),
+        "IBMPlexSans-SemiBold": require("../assets/fonts/IBMPlexSans-SemiBold.ttf"),
+        "IBMPlexSans-Bold": require("../assets/fonts/IBMPlexSans-Bold.ttf"),
+        "bebasNeueRegular": require("../assets/fonts/BebasNeue-Regular.ttf"),
     });
 
     useEffect(() => {
         if (error) throw error;
-
         if (fontsLoaded) {
             SplashScreen.hideAsync();
         }
-
-    }, []);
-
+    }, [fontsLoaded, error]);
 
     if (!fontsLoaded) {
         return null;
     }
 
-    if (!fontsLoaded && !error) {
-        return null;
-    }
-
     return (
-        <>
-            <Stack>
-                <Stack.Screen name='index' options={{ headerShown: false }} />
-                <Stack.Screen name='(auth)' options={{ headerShown: false }} />
-                <Stack.Screen name='(root)' options={{ headerShown: false }} />
-                <Stack.Screen name='(common)' options={{ headerShown: false }} />
-            </Stack>
-        </>
-    )
-}
 
-export default AppLayout
+        <AuthProvider>
+            <Stack>
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+                <Stack.Screen name="(root)" options={{ headerShown: false }} />
+                <Stack.Screen name="(common)" options={{ headerShown: false }} />
+            </Stack>
+        </AuthProvider>
+
+    );
+};
+
+export default AppLayout;
