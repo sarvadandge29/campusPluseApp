a557225addde05865dd2a4eedd78164986dcba57
sarvadandge29 <sarvadandge@gmail.com>
2025-01-30 16:41:31 +0530

    Add stats=us bar in auth layout and show password

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Stack } from 'expo-router'
+import { StatusBar } from 'react-native'
 
 const AuthLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="signIn" options={{ headerShown: false }} />
-      <Stack.Screen name="signUp" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar style="light" backgroundColor="#3B82F6" />
+
+      <Stack>
+        <Stack.Screen name="signIn" options={{ headerShown: false }} />
+        <Stack.Screen name="signUp" options={{ headerShown: false }} />
+      </Stack>
+    </>
   )
 }
 
